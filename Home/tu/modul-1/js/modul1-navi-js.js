a439f1748ab4403dea7f2141e80b7cdd69ddcf2b
nntd2703 <nntd290897@gmail.com>
2017-04-04 13:32:04 +0700

    Revert "Navigator và splashScreen"
    This reverts commit 923e9446817f967b043b9799eb07bd5020837da5.

--- Home/tu/modul-1/js/modul1-navi-js.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    $('.m1-navi-icon-mobile-btn').click(function(){
-        $('.m1-navi-menus').toggleClass('toggle-transition');
-        $('.m1-navi-menus').toggle('slow');
-    });
-});
