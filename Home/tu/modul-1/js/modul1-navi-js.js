922e0be1470149cde5f77e5387af8370771c92b7
Tấn Tú <petertu00@gmail.com>
2017-04-04 16:27:50 +0700

    Navigator and splash
    module 1 và module 0

---
+++ Home/tu/modul-1/js/modul1-navi-js.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('.m1-navi-icon-mobile-btn').click(function(){
+        $('.m1-navi-menus').toggleClass('toggle-transition');
+        $('.m1-navi-menus').toggle('slow');
+    });
+});
